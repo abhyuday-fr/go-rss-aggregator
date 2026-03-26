2a9f1c554020736d7c0d9d4c8e09d473d9db9bb8
abhyuday-fr <abhyudaypundir07@gmail.com>
2026-03-26 10:51:30 +0530

    added some database integration

--- main.go
+++ main.go
@@ -1,16 +1,23 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
 
+	"github.com/abhyuday-fr/rss-aggregator/internal/database"
 	"github.com/go-chi/chi"
 	"github.com/go-chi/cors"
 	"github.com/joho/godotenv"
-	
+	_"github.com/lib/pq"
 )
+
+type apiConfig struct{
+	DB *database.Queries
+}
+
 
 func main() {
 
@@ -19,6 +26,20 @@ func main() {
 	portString := os.Getenv("PORT") // we don't want to use 'export PORT' all the time so we are gonna get the github.com/joho/godotenv package and do 'go mod vendor'
 	if portString == "" {
 		log.Fatal("PORT is not found in the environment")
+	}
+
+	dbURL := os.Getenv("DB_URL") // we don't want to use 'export PORT' all the time so we are gonna get the github.com/joho/godotenv package and do 'go mod vendor'
+	if dbURL == "" {
+		log.Fatal("DB_URL is not found in the environment")
+	}
+
+	conn, err := sql.Open("postgres", dbURL)
+	if err != nil{
+		log.Fatal("Can't Connect to database: ", err)
+	}
+
+	apiCfg := apiConfig{
+		
 	}
 
 	//routee
@@ -51,6 +72,8 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	
 
 	fmt.Println("Port:", portString)
 }
