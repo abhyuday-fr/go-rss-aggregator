4c33206e6f27ac45a0c8be9430829a157ad08495
abhyday-fr <abhyudaypundir07@gmail.com>
2026-04-18 15:49:23 +0530

    completed full project, can now view the posts too

--- main.go
+++ main.go
@@ -71,6 +71,7 @@ func main() {
 	//to check status, mounted router
 	v1Router := chi.NewRouter()
 	v1Router.Get("/healthz", handlerReadiness) // if we use HandleFunc instead of GET then it will respond to POST too
+
 	v1Router.Get("/err", handlerErr)
 
 	v1Router.Post("/users", apiCfg.handlerCreateUser)
@@ -78,6 +79,8 @@ func main() {
 
 	v1Router.Post("/feeds", apiCfg.middlewareAuth(apiCfg.handlerCreateFeed))
 	v1Router.Get("/feeds", apiCfg.handlerGetFeeds)
+
+	v1Router.Get("/posts", apiCfg.middlewareAuth(apiCfg.handlerGetPostsForUser))
 
 	v1Router.Post("/feed_follows", apiCfg.middlewareAuth(apiCfg.handlerCreateFeedFollow))
 	v1Router.Get("/feed_follows", apiCfg.middlewareAuth(apiCfg.handlerGetFeedFollows))
